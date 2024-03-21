123a1426983cb467c0073f2c29dfa26d3c90845c
Mr-Kavikumar <ckavikumar2k@gmail.com>
2024-03-21 10:45:01 +0530

    working on dashboard page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import './App.css';
 import { UploadPage } from "../src/Component/UploadPage"
 import React from "react";
-import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import SignIn from "./Component/SignIn";
-import SignUp from "./Component/SignUp";
 
 function App() {
   return (
