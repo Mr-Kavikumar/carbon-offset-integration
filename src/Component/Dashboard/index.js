66e5d355a2eff71ab2390dff4d216e5c0d2519cd
T-abirami <ramithangaraj@gmail.com>
2024-03-29 11:20:16 +0530

    Refactor : UploadPage UI

--- src/Component/Dashboard/index.js
+++ src/Component/Dashboard/index.js
@@ -8,6 +8,7 @@ const Dashboard = () => {
     <div className="bg-gray-800 text-white w-1/5 py-6 px-6 h-screen">
       <h1 className="text-2xl font-bold  mb-4 text-left">Dashboard</h1>
       <ul className='text-left'>
+      <li className="mb-2"><a href="/uploadpage" className="text-gray-300 hover:text-white">Upload Document</a></li>
         <li className="mb-2"><a href="/communication" className="text-gray-300 hover:text-white ">Communication</a></li>
         <li className="mb-2"><a href="/Document" className="text-gray-300 hover:text-white">Document</a></li>
         <li className="mb-2"><a href="/chart" className="text-gray-300 hover:text-white">Chart</a></li>
--- src/Component/UploadPage/index.js
+++ src/Component/UploadPage/index.js
@@ -9,28 +9,17 @@ export function UploadPage() {
   const [licencebackImage, setLicencebackImage] = useState(null);
   const [insurancefrontImage, setInsurancefrontImage] = useState(null);
 
-  const handleRcbookfrontImageChange = (event) => {
-    setRcbookfrontImage(event.target.files[0]);
-  };
+  const [message, setMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleRcbookbackImageChange = (event) => {
-    setRcbookbackImage(event.target.files[0]);
-  };
-
-  const handleLicencefrontImageChange = (event) => {
-    setLicencefrontImage(event.target.files[0]);
-  };
-
-  const handleLicencebackImageChange = (event) => {
-    setLicencebackImage(event.target.files[0]);
-  };
-
-  const handleInsurancefrontImageChange = (event) => {
-    setInsurancefrontImage(event.target.files[0]);
+  const handleImageChange = (event, setImage) => {
+    setImage(event.target.files[0]);
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setIsLoading(true);
+    setMessage('Please wait...');
     try {
       const token = localStorage.getItem('token');
       const formData = new FormData();
@@ -47,8 +36,12 @@ export function UploadPage() {
         }
       });
       console.log(response.data); 
+      setMessage('Data extraction is completed...');
     } catch (error) {
       console.error(error);
+      setMessage('Failed to upload images.');
+    }finally {
+      setIsLoading(false);
     }
   };
 
@@ -56,50 +49,60 @@ export function UploadPage() {
     <div className='flex'>
       <Dashboard/>
    
-    <div className="container  mt-4 ">
-      <h2 className="text-3xl font-bold mb-8 mt-2">Upload Document</h2>
-      {/* <h3 className="text-2xl font-semibold mb-2 text-center">Select Your Images (PNG or JPEG)</h3> */}
-      <form onSubmit={handleSubmit} encType="multipart/form-data" className="flex justify-center items-center flex-col container bg-opacity-25 bg-gradient-to-r from-pink-100 via-red-100 to-red-200 p-8 rounded-lg shadow-lg" style={{ maxWidth: '400px', margin: '0 auto' }} >
-        <div className="mb-4">
-          <label htmlFor="rcbookfrontImage" className="block font-medium tracking-wide">RC Book Front Image</label>
-          <div className="mt-1 flex items-center">
-            <label htmlFor="rcbookfrontImage" className="cursor-pointer bg-gray-100 py-2 px-4 rounded-lg text-gray-600 hover:bg-gray-200">Choose File</label>
-            <input type="file" id="rcbookfrontImage" name="rcbookfrontImage" accept="image/png, image/jpeg" onChange={handleRcbookfrontImageChange} className="hidden"/>
+      <div className="container">
+        <h2 className="text-3xl font-bold mb-4 mt-2">Upload Document</h2>
+        <form onSubmit={handleSubmit} encType="multipart/form-data" className="flex justify-center items-center flex-col container bg-opacity-25 bg-gradient-to-r from-pink-100 via-red-100 to-red-200 p-8 rounded-lg shadow-lg" style={{ maxWidth: '400px', margin: '0 auto' }} >
+          <div className="mb-4">
+            <label htmlFor="rcbookfrontImage" className="block font-medium tracking-wide">RC Book Front Image</label>
+            <div className="mt-1 flex items-center">
+              <label className="cursor-pointer bg-gray-100 py-2 px-4 rounded-lg text-gray-600 hover:bg-gray-200 flex items-center">
+              <span className="file-name text-gray-600 overflow-hidden text-ellipsis whitespace-nowrap max-w-[120px] mr-2">{rcbookfrontImage ? rcbookfrontImage.name : 'chooseFile'}</span>
+                <input type="file" id="rcbookfrontImage" name="rcbookfrontImage" accept="image/png, image/jpeg" onChange={(e) => handleImageChange(e, setRcbookfrontImage)} className="hidden"/>
+              </label>
+            </div>
           </div>
-        </div>
-        <div className="mb-4 ">
-          <label htmlFor="rcbookbackImage" className="block font-medium tracking-wide">RC Book Back Image</label>
-          <div className="mt-1 flex items-center">
-            <label htmlFor="rcbookbackImage" className="cursor-pointer bg-gray-100 py-2 px-4 rounded-lg text-gray-600 hover:bg-gray-200">Choose File</label>
-            <input type="file" id="rcbookbackImage" name="rcbookbackImage" accept="image/png, image/jpeg" onChange={handleRcbookbackImageChange} className="hidden"/>
+          <div className="mb-4 ">
+  <label htmlFor="rcbookbackImage" className="block font-medium tracking-wide">RC Book Back Image</label>
+  <div className="mt-1 flex items-center">
+    <label className="cursor-pointer bg-gray-100 py-2 px-4 rounded-lg text-gray-600 hover:bg-gray-200 flex items-center" >
+      <span className="file-name text-gray-600 overflow-hidden text-ellipsis whitespace-nowrap max-w-[120px] mr-2">{rcbookbackImage ? rcbookbackImage.name : 'Choose File'}</span>
+      <input type="file" id="rcbookbackImage" name="rcbookbackImage" accept="image/png, image/jpeg" onChange={(e) => handleImageChange(e, setRcbookbackImage)} className="hidden"/>
+    </label>
+  </div>
+</div>
+
+          <div className="mb-4">
+            <label htmlFor="licencefrontImage" className="block font-medium tracking-wide">Licence Front Image</label>
+            <div className="mt-1 flex items-center">
+              <label className="cursor-pointer bg-gray-100 py-2 px-4 rounded-lg text-gray-600 hover:bg-gray-200 flex items-center">
+              <span className="file-name text-gray-600 overflow-hidden text-ellipsis whitespace-nowrap max-w-[120px] mr-2">{licencefrontImage ? licencefrontImage.name : 'chooseFile'}</span>
+                <input type="file" id="licencefrontImage" name="licencefrontImage" accept="image/png, image/jpeg" onChange={(e) => handleImageChange(e, setLicencefrontImage)} className="hidden"/>
+              </label>
+            </div>
           </div>
-        </div>
-        <div className="mb-4">
-          <label htmlFor="licencefrontImage" className="block font-medium tracking-wide">Licence Front Image</label>
-          <div className="mt-1 flex items-center">
-            <label htmlFor="licencefrontImage" className="cursor-pointer bg-gray-100 py-2 px-4 rounded-lg text-gray-600 hover:bg-gray-200">Choose File</label>
-            <input type="file" id="licencefrontImage" name="licencefrontImage" accept="image/png, image/jpeg" onChange={handleLicencefrontImageChange} className="hidden"/>
+          <div className="mb-4">
+            <label htmlFor="licencebackImage" className="block font-medium tracking-wide">Licence Back Image</label>
+            <div className="mt-1 flex items-center">
+              <label className="cursor-pointer bg-gray-100 py-2 px-4 rounded-lg text-gray-600 hover:bg-gray-200 flex items-center">
+              <span className="file-name text-gray-600 overflow-hidden text-ellipsis whitespace-nowrap max-w-[120px] mr-2">{licencebackImage ? licencebackImage.name : 'chooseFile'}</span>
+                <input type="file" id="licencebackImage" name="licencebackImage" accept="image/png, image/jpeg" onChange={(e) => handleImageChange(e, setLicencebackImage)} className="hidden"/>
+              </label>
+            </div>
           </div>
-        </div>
-        <div className="mb-4">
-          <label htmlFor="licencebackImage" className="block font-medium tracking-wide">Licence Back Image</label>
-          <div className="mt-1 flex items-center">
-            <label htmlFor="licencebackImage" className="cursor-pointer bg-gray-100 py-2 px-4 rounded-lg text-gray-600 hover:bg-gray-200">Choose File</label>
-            <input type="file" id="licencebackImage" name="licencebackImage" accept="image/png, image/jpeg" onChange={handleLicencebackImageChange} className="hidden"/>
+          <div className="mb-4 ml-6">
+            <label htmlFor="insurancefrontImage" className="block font-medium tracking-wide">Insurance Front Image</label>
+            <div className="mt-1 flex items-center">
+              <label className="cursor-pointer bg-gray-100 py-2 px-4 rounded-lg text-gray-600 hover:bg-gray-200 flex items-center">
+              <span className="file-name text-gray-600 overflow-hidden text-ellipsis whitespace-nowrap max-w-[120px] mr-2">{insurancefrontImage ? insurancefrontImage.name : 'chooseFle'}</span>
+                <input type="file" id="insurancefrontImage" name="insurancefrontImage" accept="image/png, image/jpeg" onChange={(e) => handleImageChange(e, setInsurancefrontImage)} className="hidden"/>
+              </label>
+            </div>
           </div>
-        </div>
-        <div className="mb-4 ml-6">
-          <label htmlFor="insurancefrontImage" className="block font-medium tracking-wide">Insurance Front Image</label>
-          <div className="mt-1 flex items-center">
-            <label htmlFor="insurancefrontImage" className="cursor-pointer bg-gray-100 py-2 px-4 rounded-lg text-gray-600 hover:bg-gray-200">Choose File</label>
-            <input type="file" id="insurancefrontImage" name="insurancefrontImage" accept="image/png, image/jpeg" onChange={handleInsurancefrontImageChange} className="hidden"/>
-          </div>
-        </div>
-        <button type="submit" className="bg-gray-800 hover:bg-gray-600 text-white font-bold py-2 px-2 mt-2 rounded-full tracking-wide w-1/2">Upload</button>
-      </form>
+          <button type="submit" className="bg-gray-800 hover:bg-gray-600 text-white font-bold py-2 px-2  rounded-full tracking-wide w-1/2" disabled={isLoading}>Upload</button>
+          {message && <p className="text-center mt-4">{message}</p>}
+        </form>
+      </div>
     </div>
-    </div>
-  
   );
 }
 
